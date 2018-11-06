b9c6ce8526f4bbde8ddf850ee1d24cf1189c6ff4
Cindy <cl4208@nyu.edu>
2018-11-06 10:15:30 -0500

    Set up all UI elements and score ints

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour
+{
+
+	private Scene currentScene;
+	private Scene mainMenu;
+	private Scene game;
+		
+	// Use this for initialization
+	void Start () {
+		
+		DontDestroyOnLoad(this.gameObject);
+		
+		currentScene = SceneManager.GetActiveScene();
+		mainMenu = SceneManager.GetSceneByName("Main Menu");
+		game = SceneManager.GetSceneByName("Game");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space) && currentScene == mainMenu)
+		{
+			SceneManager.LoadScene("Game");
+		}		
+		
+		
+		
+	}
+}
