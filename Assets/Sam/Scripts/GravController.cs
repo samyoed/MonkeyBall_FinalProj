8925d99d9fa198a2d48e59748d786599d94803b7
samyoed <shl638@nyu.edu>
2018-11-23 21:55:06 -0500

    Revised movement test

--- Assets/Sam/Scripts/GravController.cs
+++ Assets/Sam/Scripts/GravController.cs
@@ -15,5 +15,14 @@ public class GravController : MonoBehaviour
 	// Update is called once per frame
 	void Update () {
 		Physics.gravity = gravMult *- transform.up;
+
+		//transform.rotation = new Quaternion(transform.rotation.x, Camera.main.transform.rotation.y, transform.rotation.z, transform.rotation.w);
+		//transform.rotation = new Quaternion(0, Camera.main.transform.rotation.y,0,0);
+
+		
+		
+		
+		
+		
 	}
 }
