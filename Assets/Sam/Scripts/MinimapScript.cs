de5401dcc3143b423432616bad428b1e49799af2
samyoed <shl638@nyu.edu>
2018-11-26 23:40:02 -0500

    Added Initial Minimap and updated movement

---
+++ Assets/Sam/Scripts/MinimapScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapScript : MonoBehaviour
+{
+
+
+	public GameObject player;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+		transform.rotation = new Quaternion(transform.rotation.x,
+			Camera.main.transform.rotation.y,
+			transform.rotation.z,
+			transform.rotation.w);
+	}
+}
