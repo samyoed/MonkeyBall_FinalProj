627f4a5ba1d969f3d1299313144ff29b2bce4b81
BrendyBoi <btm299@nyu.edu>
2018-12-04 00:23:52 -0500

    The intro camera swoop is now perfected and I have added many 3d models to start to get the look of the game right.  It's now starting to feel less like a noir.

---
+++ Assets/Brendy/scrCameraTween.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrCameraTween : MonoBehaviour {
+
+    public Transform sphere, cube;
+    int GameStart;
+
+
+    void Start()
+    {
+        GameStart = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameStart += 1;
+
+        if (GameStart <= 270)
+        {
+            //Debug.Log"Floor Number";
+        }
+        if (GameStart <= 360 && GameStart >= 271)
+        {
+            //Debug.Log"Ready";
+        }
+
+        if (GameStart >= 360)
+        { 
+        // x += (target - x) * 0.1
+        // every frame, move 10% of the remaining distance;
+        sphere.position += (cube.position - sphere.position) * 0.1f;
+        }
+
+    }
+}
