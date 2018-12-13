3e7bdee08dcc0d872aafb8a320b9a5a3d6330922
samyoed <shl638@nyu.edu>
2018-12-13 11:29:08 -0500

    Added two more levels, implemented models, added collider to goal post

---
+++ Assets/MapSpin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSpin : MonoBehaviour
+{
+	public float rotSpeed = 0;
+	//public Vector3 mapCenter;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	
+		RotateInPlace();
+		
+		
+	}
+	
+	void RotateInPlace()
+	{
+		transform.Rotate(0, rotSpeed * Time.deltaTime, 0, Space.World);
+	}
+	
+}
