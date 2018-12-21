e64b41114290e25b6df75b5c62fc06b260d17bb2
MC <mcmailbox014@gmail.com>
2018-12-20 21:26:45 -0500

    Credits done

---
+++ Assets/creditsScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class creditsScene : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			SceneManager.LoadScene ("Main Menu");
+		}
+	}
+}
